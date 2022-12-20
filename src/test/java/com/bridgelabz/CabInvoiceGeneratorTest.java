6ea39bc6e5d763988da438c80f754f46e4c1fa2d
Surabhi951 <surabhibhagat95@gmail.com>
2022-12-20 13:44:40 +0530

    Modified: Multiple Rides

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -15,6 +15,15 @@ public class CabInvoiceGeneratorTest {
         Assertions.assertEquals(65, fare);//Verifies that the expected and actual values are equal
     }
 
+    //calculate minimum fare
+    @Test
+    public void givenMinDistanceAndTime_ShouldReturn_MinFare() {
+        double distance = 1.5;
+        int time = 5;
+        double fare = cabInvoiceGenerator.calculateFare(distance, time);//Using object reference calling calculateFare method with parameters
+        Assertions.assertEquals(20, fare);//Verifies that the expected and actual values are equal
+    }
+
     @Test
     public void givenMultipleRides_ShouldReturn_TotalFare() {//Total fare for multiple rides
         Ride[] rides = {
